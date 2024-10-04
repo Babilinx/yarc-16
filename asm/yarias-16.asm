f81346f6a201fcc39749f91c7f3bf48f9435ff68
babilinx <babilinx.evx1o@simpelogin.com>
2024-10-04 18:42:28 +0200

    feat(!isa): total rework of the ISA and register placement

--- asm/yarias-16.asm
+++ asm/yarias-16.asm
@@ -1,209 +1,129 @@
 #subruledef reg
 {
-    r0 => 0x0
-    r1 => 0x1
-    r2 => 0x2
-    r3 => 0x3
-    r4 => 0x4
-    r5 => 0x5
-    r6 => 0x6
-    r7 => 0x7
-    r8 => 0x8
-    r9 => 0x9
-    r10 => 0xA
-    r11 => 0xB
-    r12 => 0xC
-    r13 => 0xD
-    r14 => 0xE
-    r15 => 0xF
     zero => 0x0
-    a0 => 0x1
+    ra => 0x1
     a1 => 0x2
     a2 => 0x3
-    s0 => 0x4
-    fp => 0x4
-    s1 => 0x5
-    s2 => 0x6
-    s3 => 0x7
-    s4 => 0x8
-    s5 => 0x9
-    t0 => 0xA
-    t1 => 0xB
-    t2 => 0xC
-    t3 => 0xD
-    ra => 0xE
-    sp => 0xF
+    t1 => 0x4
+    t3 => 0x5
+    s4 => 0x6
+    s5 => 0x7
+    sp => 0x8
+    a0 => 0x9
+    s0 => 0xA
+    fp => 0xA
+    s1 => 0xB
+    s2 => 0xC
+    s3 => 0xD
+    t0 => 0xE
+    t4 => 0xF
+}
+
+#subruledef sreg  ; for instructions with 3 bit reg field
+{
+    sp => 0x0`3
+    a0 => 0x1`3
+    s0 => 0x2`3
+    fp => 0x2`3
+    s1 => 0x3`3
+    s2 => 0x4`3
+    s3 => 0x5`3
+    t0 => 0x6`3
+    t4 => 0x7`3
+}
+
+#ruledef 
+{
+    r_fmt {op: u2}, {funct5: u5}, {rs1d: reg}, {rs2: reg} => 0x0`1 @ funct5 @ rs1d @ rs2 @ op
+    i_fmt {op: u2}, {funct3: u3}, {rs1d: reg}, {imm: u7} => funct3 @ imm[6:4] @ rs1d @ imm[3:0] @ op
+    sr_fmt {op: u2}, {funct3: u3}, {rs2: reg}, {imm: u7} => funct3 @ imm @ rs2d @ op
+    wi_fmt {op: u2}, {funct3: u3}, {rd: sreg}, {imm: u8} => funct3 @ imm @ rd @ op
+    l_fmt {op: u2}, {funct3: u3}, {rd: sreg}, {imm: u5}, {rs1: sreg} => funct3 @ imm[4:1] @ rs1 @ imm[0:0] @ rd @ op
+    s_fmt {op: u2}, {funct3: u3}, {imm: u5}, {rs1: sreg}, {rs2: sreg} => funct3 @ imm[4:1] @ rs1 @ imm[0:0] @ rs2 @ op
+    b_fmt {op: u2}, {funct3: u3}, {rs1: sreg}, {imm: u8} => funct3 @ imm[7:4] @ rs1 @ imm[3:0] @ op
+    j_fmt {op: u2}, {funct3: u3}, {imm: u11} => funct3 @ imm @ op
 }
 
 #ruledef
 {
-    ;; base instructions
-    ; ALU
-    add {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x0 @ 0x0
-    sub {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x1 @ 0x0
-    mul {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x2 @ 0x0
-    div {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x3 @ 0x0
-    mod {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x4 @ 0x0
-    and {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x5 @ 0x0
-    or  {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x6 @ 0x0
-    xor {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x7 @ 0x0
-    not {rs1: reg}             => rs2 @ rs1 @ 0x8 @ 0x0
-    lsh {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0x9 @ 0x0
-    rsh {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0xA @ 0x0
-    cmp {rs1: reg}, {rs2: reg} => rs2 @ rs1 @ 0xF @ 0x0
-    addi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x0 @ 0x1
-    subi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x1 @ 0x1
-    muli {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x2 @ 0x1
-    divi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x3 @ 0x1
-    modi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x4 @ 0x1
-    andi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x5 @ 0x1
-    ori  {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x6 @ 0x1
-    xori {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x7 @ 0x1
-    lshi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0x9 @ 0x1
-    rshi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0xA @ 0x1
-    cmpi {rs1: reg}, {imm: u4} => imm @ rs1 @ 0xF @ 0x1
+    ;; register type
+    mv {rd: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x00`5, {rd}, {rs2}}
     
-    ; memory
-    lb {rd: reg}, {imm: u4}({rs1: reg})  => imm @ rs1 @ rd @ 0x4
-    lw {rd: reg}, {imm: u4}({rs1: reg})  => imm @ rs1 @ rd @ 0x6
-    sb {imm: u4}({rs1: reg}), {rs2: reg} => rs2 @ rs1 @ imm @ 0x5
-    sw {imm: u4}({rs1: reg}), {rs2: reg} => rs2 @ rs1 @ imm @ 0x7
+    slt {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x01`5, {rs1d}, {rs2}}
+    slu {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x02`5, {rs1d}, {rs2}}
+    seq {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x03`5, {rs1d}, {rs2}}
+    sne {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x04`5, {rs1d}, {rs2}}
+    slte {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x05`5, {rs1d}, {rs2}}
+    sleu {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x06`5, {rs1d}, {rs2}}
     
-    ; registers
-    ln {rd: reg}              => 0x0 @ 0x0 @ rd @ 0x2
-    mov {rd: reg}, {rs1: reg} => 0x0 @ rs1 @ rd @ 0x3
+    add {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x10`5, {rs1d}, {rs2}}
+    sub {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x11`5, {rs1d}, {rs2}}
+    and {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x12`5, {rs1d}, {rs2}}
+    lls {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x13`5, {rs1d}, {rs2}}
+    rls {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x14`5, {rs1d}, {rs2}}
+    ras {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x15`5, {rs1d}, {rs2}}
+    xor {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x16`5, {rs1d}, {rs2}}
+    or  {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x17`5, {rs1d}, {rs2}}
+    not {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x18`5, {rs1d}, {rs2}}
+    mul {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x19`5, {rs1d}, {rs2}}
+    div {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x1A`5, {rs1d}, {rs2}}
+    rem {rs1d: reg}, {rs2: reg} => asm {r_fmt 0`2, 0x1B`5, {rs1d}, {rs2}}
     
-    ; branching
-    br  {imm: s9}({rs1: reg}) => imm[8:5] @ rs1 @ imm[4:1] @ 0xA
-    blt {imm: s9}({rs1: reg}) => imm[8:5] @ rs1 @ imm[4:1] @ 0xB
-    blu {imm: s9}({rs1: reg}) => imm[8:5] @ rs1 @ imm[4:1] @ 0xC
-    bnz {imm: s9}({rs1: reg}) => imm[8:5] @ rs1 @ imm[4:1] @ 0xD
-    jmp {imm: s9}             => imm[8:5] @ 0x0 @ imm[4:1] @ 0xE
-    jlr {rs1: reg}            => 0x0 @ rs1 @ 0x0 @ 0xF
+    ;; immediate type
+    addi {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x0`3, {rs1d}, {imm}}
+    subi {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x1`3, {rs1d}, {imm}}
+    andi {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x2`3, {rs1d}, {imm}}
+    llsi {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x3`3, {rs1d}, {imm}}
+    rlsi {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x4`3, {rs1d}, {imm}}
+    rasi {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x5`3, {rs1d}, {imm}}
+    
+    slti {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x6`3, {rs1d}, {imm}}
+    slui {rs1d: reg}, {imm: u7} => asm {i_fmt 1`2, 0x7`3, {rs1d}, {imm}}
+    
+    jr {imm: u8}({rs1: reg}) => asm {i_fmt 2`2, 0x0`3, {rs1}, {imm[7:1]}}
+    jlr {imm: u8}({rs1: reg}) => asm {i_fmt 3`2, 0x0`3, {rs1}, {imm[7:1]}}
+    
+    ;; stack relative type
+    swsp {rd: reg}, {imm: u7} => asm {sr_fmt 3`2, 0x1`3, {imm}, {rd}}
+    lwsp {imm: u7}, {rs2} => asm {sr_fmt 2`2, 0x0`3, {imm}, {rs2}}
+    
+    ;; wide immediate type
+    li {rd: sreg}, {imm: u8} => asm {wi_fmt 2`2, 0x0`3, {rd}, {imm}}
+    lui {rd: sreg}, {imm: u8} => asm {wi_fmt 2`2, 0x0`3, {rd}, {imm}}
+    adduipc {rd: sreg}, {imm: u8} => asm {wi_fmt 2`2, 0x0`3, {rd}, {imm}}
+    addisp {rd: sreg}, {imm: u8} => asm {wi_fmt 2`2, 0x0`3, {rd}, {imm}}
+    
+    ;; load type
+    lb {rd: sreg}, {imm: u5}({rs1: sreg}) => asm {l_fmt 3`2, 0x2`3, {rd}, {imm}, {rs1}}
+    lw {rd: sreg}, {imm: u5}({rs1: sreg}) => asm {l_fmt 3`2, 0x3`3, {rd}, {imm}, {rs1}}
+    
+    ;; store type
+    sb {imm: u5}({rs1: sreg}), {rs2: sreg} => asm {s_fmt 3`2, 0x4`3, {imm}, {rs1}, {rs2}}
+    sw {imm: u5}({rs1: sreg}), {rs2: sreg} => asm {s_fmt 3`2, 0x5`3, {imm}, {rs1}, {rs2}}
+    
+    ;; branch type
+    bz {imm: u9}, {rs1: sreg} => asm {b_fmt 3`2, 0x6`3, {rs1}, {imm[8:1]}}
+    bz {imm: u9}, {rs1: sreg} => asm {b_fmt 3`2, 0x7`3, {rs1}, {imm[8:1]}}
+    
+    ;; jump type
+    jmp {imm: u12} => asm {j_fmt 2`2, 0x6`3, {imm[11:1]}}
+    jal {imm: u12} => asm {j_fmt 2`2, 0x6`3, {imm[11:1]}}
 }
 
 #ruledef
 {
     ;; pseudoinstructions
-    ; ALU
-    add {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x0 @ 0x1
-    sub {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x1 @ 0x1
-    mul {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x2 @ 0x1
-    div {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x3 @ 0x1
-    mod {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x4 @ 0x1
-    and {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x5 @ 0x1
-    or  {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x6 @ 0x1
-    xor {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x7 @ 0x1
-    lsh {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0x9 @ 0x1
-    rsh {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0xA @ 0x1
-    cmp {rs1: reg}, {imm: u4}  => imm @ rs1 @ 0xF @ 0x1
     
-    ; branching
-    br {imm: u16} =>
-    {
-        reladdr = {imm} - $ - 2
-        ; Using 'jmp' in this case saves 12 bytes of memory
-        assert(reladdr >=  0xff, "Immediate too small. Use 'jmp'")
-        assert(reladdr >= !0xff, "Immediate too small. Use 'jmp'")
-        asm
-        {
-            li t3, {imm}
-            br 0(t3)
-        } 
-    }
-    blt {imm: u16} =>
-    {
-        reladdr = imm - $ - 2
-        assert(reladdr <=  0xff)
-        assert(reladdr >= !0xff)
-        asm {blt ({imm} - $ - 2)`9(zero)}
-    }
-    blt {imm: u16} => asm
-    {
-        li t3, {imm}
-        blt 0(t3)
-    }
-    blt {imm: u16}, {rs1: reg}, {rs2: reg} => asm
-    {
-        cmp {rs1}, {rs2}
-        blt {imm}
-    }
-    blu {imm: u16} =>
-    {
-        reladdr = imm - $ - 2
-        assert(reladdr <=  0xff)
-        assert(reladdr >= !0xff)
-        asm {blu ({imm} - $ - 2)`9(zero)}
-    }
-    blu {imm: u16} => asm
-    {
-        li t3, {imm}
-        blu 0(t3)
-    }
-    blu {imm: u16}, {rs1: reg}, {rs2: reg} => asm
-    {
-        cmp {rs1}, {rs2}
-        blu {imm}
-    }
-    bnz {imm: u16} =>
-    {
-        reladdr = imm - $ - 2
-        assert(reladdr <=  0xff)
-        assert(reladdr >= !0xff)
-        asm {bnz ({imm} - $ - 2)`9(zero)}
-    }
-    
-    bnz {imm: u16} => asm
-    {
-        li t3, {imm}
-        bnz 0(t3)
-    }
-    bnz {imm: u16}, {rs1: reg} => asm
-    {
-        cmp {rs1}, zero
-        bnz {imm}
-    }
-    bgt {imm: u16}, {rs1: reg}, {rs2: reg} => asm
-    {
-        cmp {rs2}, {rs1}
-        blt {imm}
-    }
-    bgu {imm: u16}, {rs1: reg}, {rs2: reg} => asm
-    {
-        cmp {rs2}, {rs1}
-        blu {imm}
-    }
-    jlr {imm: u16} => asm
-    {
-        li t3, {imm}
-        jlr t3
-    }
     jmp {imm: u16} =>  ; only local jmp
     {
-        reladdr = imm - $ - 2
-        assert(reladdr >=  0xff, "Immediate too big. Use 'br'")
-        assert(reladdr <= !0xff, "Immediate too bit. Use 'br'")
-        asm {jmp ({imm} - $ - 2)`9}
+        assert(imm >=  0xF000)
+        asm {jmp {imm[11:1]}}
     }
-}
-
-#ruledef
-{	
-    ; macros
-    li {rd: reg}, {imm: u16} => asm {ln t0} @ {imm} @ asm {sw 0({rd}), t0}
-
-    pop {rd: reg} => asm
+    jmp {imm: u16} => asm
     {
-        subi sp, 2
-        lw {rd}, 0(sp)
+        lui t4, imm[15:8]
+        jr imm[7:0](t4)
     }
-    
-    push {rs1: reg} => asm
-    {
-        sw 0(sp), {rs1}
-        addi sp, 2
-    }
-    
-    nop => 0x0000
+
 }
 
